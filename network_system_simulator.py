859fc8136605b69ceb9219ffd1079fa4be15afc7
Swargo <rs75c@rs5wmbg24.managed.mst.edu>
2024-11-13 12:55:32 -0600

    state tracking added

--- network_system_simulator.py
+++ network_system_simulator.py
@@ -3,7 +3,7 @@ import numpy as np
 from queue import PriorityQueue
 
 class NetworkSystemSimulator:
-    def __init__(self, read_thread = 1, network_thread = 1, write_thread = 1, sender_buffer_capacity = 10, receiver_buffer_capacity = 10, read_throughput_per_thread = 3, write_throughput_per_thread = 3, network_throughput_per_thread = 1, read_bandwidth = 6, write_bandwidth = 6, network_bandwidth = 6, read_background_traffic = 0, write_background_traffic = 0, network_background_traffic = 0):
+    def __init__(self, read_thread = 1, network_thread = 1, write_thread = 1, sender_buffer_capacity = 10, receiver_buffer_capacity = 10, read_throughput_per_thread = 3, write_throughput_per_thread = 1, network_throughput_per_thread = 2, read_bandwidth = 6, write_bandwidth = 6, network_bandwidth = 6, read_background_traffic = 0, write_background_traffic = 0, network_background_traffic = 0, track_states = False):
         self.sender_buffer_capacity = sender_buffer_capacity
         self.receiver_buffer_capacity = receiver_buffer_capacity
         self.read_throughput_per_thread = read_throughput_per_thread
@@ -18,30 +18,45 @@ class NetworkSystemSimulator:
         self.read_thread = read_thread
         self.network_thread = network_thread
         self.write_thread = write_thread
+        self.track_states = track_states
         self.K = 1.02
         
         # Initialize the buffers
         self.sender_buffer_in_use = max(min(self.read_throughput_per_thread * read_thread - self.network_throughput_per_thread * self.network_thread, self.sender_buffer_capacity), 0)
         self.receiver_buffer_in_use = max(min(self.network_throughput_per_thread * network_thread - self.write_throughput_per_thread * self.write_thread, self.receiver_buffer_capacity), 0)
 
+        print(f"Initial Sender Buffer: {self.sender_buffer_in_use}, Receiver Buffer: {self.receiver_buffer_in_use}")
+
+
+        if self.track_states:
+            with open('optimizer_call_level_states.csv', 'w') as f:
+                f.write("Read Thread, Network Thread, Write Thread, Utility, Read Throughput, Sender Buffer, Network Throughput, Receiver Buffer, Write Throughput\n")
+            
+            with open('thread_level_states.csv', 'w') as f:
+                f.write("Thread Type, Throughput, Sender Buffer, Receiver Buffer\n")
+                f.write(f"Initial, 0, {self.sender_buffer_in_use}, {self.receiver_buffer_in_use}\n")
+
     def read_thread_task(self, time):
-        read_throughput_temp = 0
-        # print(f"Read Thread start: Sender Buffer: {self.sender_buffer_in_use}, Receiver Buffer: {self.receiver_buffer_in_use}")
+        throughput_increase = 0
         if self.sender_buffer_in_use < self.sender_buffer_capacity:
             read_throughput_temp = min(self.read_throughput_per_thread, self.sender_buffer_capacity - self.sender_buffer_in_use)
             throughput_increase = min(read_throughput_temp, self.read_bandwidth-self.read_throughput)
             self.read_throughput += throughput_increase
             self.sender_buffer_in_use += throughput_increase
 
-        time_taken = read_throughput_temp / self.read_throughput_per_thread
+        time_taken = throughput_increase / self.read_throughput_per_thread
         next_time = time + time_taken + 0.00001
         if next_time < 1:
             self.thread_queue.put((next_time, "read"))
-        # print(f"Read Thread end: Sender Buffer: {self.sender_buffer_in_use}, Receiver Buffer: {self.receiver_buffer_in_use}")
+        
+        if throughput_increase > 0 and self.track_states:
+            with open('thread_level_states.csv', 'a') as f:
+                f.write(f"Read, {throughput_increase}, {self.sender_buffer_in_use}, {self.receiver_buffer_in_use}\n")
         return next_time
 
     def network_thread_task(self, time):
-        network_throughput_temp = 0
+        throughput_increase = 0
+        # print(f"Network Thread start: Network Throughput: {throughput_increase}, Sender Buffer: {self.sender_buffer_in_use}, Receiver Buffer: {self.receiver_buffer_in_use}")
         if self.sender_buffer_in_use > 0 and self.receiver_buffer_in_use < self.receiver_buffer_capacity:
             network_throughput_temp = min(self.network_throughput_per_thread, self.sender_buffer_in_use, self.receiver_buffer_capacity - self.receiver_buffer_in_use)
             throughput_increase = min(network_throughput_temp, self.network_bandwidth-self.network_throughput)
@@ -49,26 +64,32 @@ class NetworkSystemSimulator:
             self.sender_buffer_in_use -= throughput_increase
             self.receiver_buffer_in_use += throughput_increase
 
-        time_taken = network_throughput_temp / self.network_throughput_per_thread
+        time_taken = throughput_increase / self.network_throughput_per_thread
         next_time = time + time_taken + 0.00001
         if next_time < 1:
             self.thread_queue.put((next_time, "network"))
-        # print(f"Network Thread: Network Throughput: {network_throughput_temp}, Sender Buffer: {self.sender_buffer_in_use}, Receiver Buffer: {self.receiver_buffer_in_use}")
+        # print(f"Network Thread end: Network Throughput: {throughput_increase}, Sender Buffer: {self.sender_buffer_in_use}, Receiver Buffer: {self.receiver_buffer_in_use}")
+        if throughput_increase > 0 and self.track_states:
+            with open('thread_level_states.csv', 'a') as f:
+                f.write(f"Network, {throughput_increase}, {self.sender_buffer_in_use}, {self.receiver_buffer_in_use}\n")
         return next_time
 
     def write_thread_task(self, time):
-        write_throughput_temp = 0
+        throughput_increase = 0
         if self.receiver_buffer_in_use > 0:
             write_throughput_temp = min(self.write_throughput_per_thread, self.receiver_buffer_in_use)
             throughput_increase = min(write_throughput_temp, self.write_bandwidth-self.write_throughput)
             self.write_throughput += throughput_increase
             self.receiver_buffer_in_use -= throughput_increase
 
-        time_taken = write_throughput_temp / self.write_throughput_per_thread
+        time_taken = throughput_increase / self.write_throughput_per_thread
         next_time = time + time_taken + 0.00001
         if next_time < 1:
             self.thread_queue.put((next_time, "write"))
         # print(f"Write Thread: Sender Buffer: {self.sender_buffer_in_use}, Receiver Buffer: {self.receiver_buffer_in_use}")
+        if throughput_increase > 0 and self.track_states:
+            with open('thread_level_states.csv', 'a') as f:
+                f.write(f"Write, {throughput_increase}, {self.sender_buffer_in_use}, {self.receiver_buffer_in_use}\n")
         return next_time
     
     def get_utility_value(self, threads):
@@ -112,8 +133,12 @@ class NetworkSystemSimulator:
 
         utility = (self.read_throughput/self.K ** read_thread) + (self.network_throughput/self.K ** network_thread) + (self.write_throughput/self.K ** write_thread)
 
-        print(f"Read thread: {read_thread}, Network thread: {network_thread}, Write thread: {write_thread}, Utility: {utility}, Read throughput: {self.read_throughput}, Sender Buffer: {self.sender_buffer_in_use}, Network throughput: {self.network_throughput}, Receiver Buffer: {self.receiver_buffer_in_use}, Write throughput: {self.write_throughput}")
-
+        print(f"Read thread: {read_thread}, Network thread: {network_thread}, Write thread: {write_thread}, Utility: {utility}")
+        
+        if self.track_states:
+            with open('optimizer_call_level_states.csv', 'a') as f:
+                f.write(f"{read_thread}, {network_thread}, {write_thread}, {utility}, {self.read_throughput}, {self.sender_buffer_in_use}, {self.network_throughput}, {self.receiver_buffer_in_use}, {self.write_throughput}\n")
+        
         return utility * -1
     
     def optimize_threads(self, init_threads=(1, 1, 1)):
